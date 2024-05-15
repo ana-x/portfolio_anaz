3bae71d7d8c609f84a0d57d29ab4cf7a6355462f
anas <anazmuhammed2003@gmail.com>
2024-05-15 20:49:40 +0530

    firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'features/home_page/screens/home_slider.dart';
+import 'firebase_options.dart';
 
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
